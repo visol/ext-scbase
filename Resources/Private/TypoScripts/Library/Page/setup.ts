60bf326aa341e4f94fd038ba8a0afd37c785f32a
Jonas Renggli <jonas.renggli@swisscom.com>
2014-11-24 18:08:14 +0000

    [FEATURE] Add rendering for BackendLayouts

--- Resources/Private/TypoScripts/Library/Page/setup.ts
+++ Resources/Private/TypoScripts/Library/Page/setup.ts
@@ -66,7 +66,17 @@ page {
 
 	10 = FLUIDTEMPLATE
 	10 {
-		file = {$PATH.privatePath}Templates/index.html
+		file.stdWrap.cObject = TEXT
+		file.stdWrap.cObject {
+			data = levelfield:-2,backend_layout_next_level,slide
+			override.field = backend_layout
+			split {
+				token = file__
+				1.current = 1
+				1.wrap = |
+			}
+			wrap = {$PATH.privatePath}Templates/|.html
+		}
 		partialRootPath = {$PATH.privatePath}Partials/
 		layoutRootPath = {$PATH.privatePath}Layouts/
 
