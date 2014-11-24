0470b9be0ed79975fdec47c64f16e50d72ba579b
Daniel Huf <daniel.huf@swisscom.com>
2014-11-24 14:17:52 +0000

    [TASK] Hide missing accesskeys wrap if no uid defined

--- Resources/Private/TypoScripts/Library/Menu/AccessKeys/setup.ts
+++ Resources/Private/TypoScripts/Library/Menu/AccessKeys/setup.ts
@@ -3,18 +3,21 @@ lib.scbase.menu.accesskeys {
 	wrap = <ul id="accesskeys">|</ul>
 	10 = TEXT
 	10 {
-		data = LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey0
+		stdWrap.if.isTrue.data = {$PID.homepage}
+		data = LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey0
 		typolink {
 			parameter = {$PID.homepage}
 			ATagParams= accesskey="0"
 			title.cObject = TEXT
-			title.cObject.data = LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey0
+			title.cObject.data = LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey0
+			wrap = <li>|</li>
+			required = 1
 		}
-		wrap = <li>|</li>
+
 	}
 	20 = TEXT
 	20 {
-		data = LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey1
+		data = LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey1
 		typolink {
 			parameter.data = TSFE:id
 			addQueryString = 1
@@ -22,47 +25,48 @@ lib.scbase.menu.accesskeys {
 			section = {$site.id.mainNavigation}
 			ATagParams= accesskey="1"
 			title.cObject = TEXT
-			title.cObject.data = LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey1
+			title.cObject.data = LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey1
+			wrap = <li>|</li>
+			required = 1
 		}
-		wrap = <li>|</li>
 	}
 	30 < .20
 	30 {
-		data = LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey2
+		data = LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey2
 		typolink {
 			ATagParams= accesskey="2"
 			section = {$site.id.mainContent}
-			title.cObject.data = LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey2
+			title.cObject.data = LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey2
 		}
 	}
 	40 < .10
 	40 {
 		stdWrap.if.isTrue.data = {$PID.contact}
-		data = LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey3
+		data = LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey3
 		typolink {
 			parameter = {$PID.contact}
 			ATagParams= accesskey="3"
-			title.cObject.data =  LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey3
+			title.cObject.data =  LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey3
 		}
 	}
 	50 < .10
 	50 {
 		stdWrap.if.isTrue.data = {$PID.sitemap}
-		data = LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey4
+		data = LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey4
 		typolink {
 			parameter = {$PID.sitemap}
 			ATagParams= accesskey="4"
-			title.cObject.data = LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey4
+			title.cObject.data = LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey4
 		}
 	}
 	60 < .10
 	60 {
 		stdWrap.if.isTrue.data = {$PID.search}
-		data = LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey5
+		data = LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey5
 		typolink {
 			parameter = {$PID.search}
 			ATagParams= accesskey="5"
-			title.cObject.data = LLL:{$FILE.translationFile}:pageElement.navAccesskey.accesskey5
+			title.cObject.data = LLL:{$PATH.privatePath}Language/locallang.xml:pageElement.navAccesskey.accesskey5
 		}
 	}
 }
