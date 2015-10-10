cb6806e9248ff1b99c9788b8817125bea408c72e
Daniel Huf <daniel.huf@swisscom.com>
2015-10-10 14:57:15 +0000

    [TASK] Clean up the lib.scbase.menu.footer and add the constant

--- Resources/Private/TypoScripts/Library/Menu/Footer/setup.ts
+++ Resources/Private/TypoScripts/Library/Menu/Footer/setup.ts
@@ -1,14 +1,7 @@
 lib.scbase.menu.footer < lib.scbase.menu.template
 lib.scbase.menu.footer {
 	special = directory
-	special.value = 8
-
+	special.value = {$PID.menu.footer}
 	entryLevel = 0
-
-	1 {
-
-	}
-
-	#stdWrap.prepend.data = LLL:{$filepaths.l10n}:nav_top
-	stdWrap.prepend = Footernavigation
+	1.wrap = <ul>|</ul>
 }
