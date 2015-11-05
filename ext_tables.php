523e9ed8708a727ea25237106109a6b2ca75324f
Daniel Huf <daniel.huf@swisscom.com>
2015-11-05 15:35:32 +0000

    [TASK] Add compatibility for 4.5

--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,8 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScripts', 'scbase template');
+if (version_compare(TYPO3_branch, '6.2', '>=')) {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScripts', 'scbase template');
+}else{
+	t3lib_extMgm::addStaticFile($_EXTKEY, 'Resources/Private/TypoScripts', 'scbase template');
+}
